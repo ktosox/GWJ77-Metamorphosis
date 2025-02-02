f21c83d6a345886aa2d1559e0fed2c52b738164c
Ktosox <ktoso3@gmail.com>
2025-02-02 13:57:04 +0100

    more drafting before work starts

--- Game/level/segments/grid_map_segment.gd
+++ Game/level/segments/grid_map_segment.gd
@@ -1,8 +1,12 @@
 extends GridMap
 
-# creates an 8x8 tunnel out of 2x2x4 mesh sections using an WFC algorithm
-# start and end points are assumed as determined
+# creates an 8x8 tunnel of arbitrary length out of 2x2x4 mesh sections using an WFC algorithm
+# either start, end, or start + end points are assumed as determined when the generation begins
 # the process consits of 3 parts: observe, collpase, propagate
-# observe picks the next thing
+# observe picks the next thing by finding the thing with "lowest entropy"
 # collapse turns a fuzzy thing into a specific thing
 # propagate passes on data to the neighbourse of the collpased thing
+
+# at start an Array of all the un-collapsed spots needs to be created - AKA the Wave
+# then a series of propagations outside the normal flow needs to be completed in order "reduce entropy" for the spots that touch pre-determined tiles
+# then the real fun starts with the loop that begins with "observe" and keeps going till the Wave is empty
