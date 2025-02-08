d3327e63b68abf503e633d514fd180687a4cb32c
Ktosox <ktoso3@gmail.com>
2025-02-08 19:40:35 +0100

    the code that finds all of the linked neighbor tiles looks like poop
    BUT IT WORKS!

--- Game/level/segments/grid_map_segment.gd
+++ Game/level/segments/grid_map_segment.gd
@@ -16,6 +16,8 @@ extends GridMap
 
 
 class Tile:
+	var location : Vector3i
+	var orientation : int
 	var neighbours : Array[Vector3i]
 	var state : int
 	var valid_states = [0,1,2]
@@ -41,17 +43,84 @@ var entropy_tier_list = { # pairs entropy size as int with an Array of Tile with
 } 
 
 var all_valid_sections = {# a dictionary describing all 4 walls, where keys are the orientations and arrays have 4 cells each / y 
+	3 : [Vector3i(-2,0,2),Vector3i(-2,0,1),Vector3i(-2,0,0),Vector3i(-2,0,-1)],
 	11: [Vector3i(3,0,-1),Vector3i(3,0,0),Vector3i(3,0,1),Vector3i(3,0,2)],
-	3 : [Vector3i(-2,0,-1),Vector3i(-2,0,0),Vector3i(-2,0,1),Vector3i(-2,0,2)],
+	15 : [Vector3i(2,0,3),Vector3i(1,0,3),Vector3i(0,0,3),Vector3i(-1,0,3)],
 	7 : [Vector3i(-1,0,-2),Vector3i(0,0,-2),Vector3i(1,0,-2),Vector3i(2,0,-2)],
-	15 : [Vector3i(-1,0,3),Vector3i(0,0,3),Vector3i(1,0,3),Vector3i(2,0,3)],
 	
 	}
+
+func find_linked_location(location : Vector3i) -> Array:
+	var neigbours = []
+	var base_layer = []
+	if location.x == -2:
+		if location.z == 2: # we need to teleoprt to a diffrent section
+			base_layer.push_back(location)
+			base_layer.push_back(location + Vector3i(0,0,-1))
+			base_layer.push_back(Vector3i(-1,0,3) + Vector3i(0,location.y,0))
+		elif location.z == -1: # we need to teleoprt to a diffrent section
+			base_layer.push_back(location)
+			base_layer.push_back(location + Vector3i(0,0,1))
+			base_layer.push_back(Vector3i(-1,0,-2) + Vector3i(0,location.y,0))
+		else: # we stay in the same section
+			for n in [-1,0,1]: 
+				base_layer.push_back(location + Vector3i(0,0,n))
+	if location.x == 3:
+		if location.z == 2: # we need to teleoprt to a diffrent section
+			base_layer.push_back(location)
+			base_layer.push_back(location + Vector3i(0,0,-1))
+			base_layer.push_back(Vector3i(2,0,3) + Vector3i(0,location.y,0))
+		elif location.z == -1: # we need to teleoprt to a diffrent section
+			base_layer.push_back(location)
+			base_layer.push_back(location + Vector3i(0,0,1))
+			base_layer.push_back(Vector3i(2,0,-2) + Vector3i(0,location.y,0))
+		else: # we stay in the same section
+			for n in [-1,0,1]: 
+				base_layer.push_back(location + Vector3i(0,0,n))
+	if location.z == -2:
+		if location.x == 2: # we need to teleoprt to a diffrent section
+			base_layer.push_back(location)
+			base_layer.push_back(location + Vector3i(-1,0,0))
+			base_layer.push_back(Vector3i(3,0,-1) + Vector3i(0,location.y,0))
+		elif location.x == -1: # we need to teleoprt to a diffrent section
+			base_layer.push_back(location)
+			base_layer.push_back(location + Vector3i(1,0,0))
+			base_layer.push_back(Vector3i(-2,0,-1) + Vector3i(0,location.y,0))
+		else: # we stay in the same section
+			for n in [-1,0,1]: 
+				base_layer.push_back(location + Vector3i(n,0,0))
+	if location.z == 3:
+		if location.x == 2: # we need to teleoprt to a diffrent section
+			base_layer.push_back(location)
+			base_layer.push_back(location + Vector3i(-1,0,0))
+			base_layer.push_back(Vector3i(3,0,2) + Vector3i(0,location.y,0))
+		elif location.x == -1: # we need to teleoprt to a diffrent section
+			base_layer.push_back(location)
+			base_layer.push_back(location + Vector3i(1,0,0))
+			base_layer.push_back(Vector3i(-2,0,2) + Vector3i(0,location.y,0))
+		else: # we stay in the same section
+			for n in [-1,0,1]: 
+				base_layer.push_back(location + Vector3i(n,0,0))
+
+	for n in [-1,0,1]:
+		for m in base_layer:
+			neigbours.push_back(m + Vector3i(0,n,0))
+	neigbours.erase(location)
+
+	return neigbours
 
 func _ready() -> void:
 	for cell in get_used_cells():
 		set_cell_item(cell,-1)
-	
+	find_linked_location(Vector3i(-2,10,2))
+	#var iteration = 0
+	#for section in all_valid_sections:
+		#
+		#for location in all_valid_sections[section]:
+			#
+			#set_cell_item(location,iteration,section)
+		#iteration += 1
+	#create_empty_tiles(-6,2)
 
 
 	#for z in 24:
@@ -60,17 +129,27 @@ func _ready() -> void:
 
 func create_empty_tiles(from : int, to : int) -> void: # creates an Array of Vector3i represeting locations on GridMap between "from" and "to" (inclusive)
 	assert(from < to)
-	var array_of_locations = []
+	var list_of_locations_by_Y : Dictionary
 	var current_floor = from
 	while current_floor <= to: # populate array_of_locations wit hall of the locations
+		list_of_locations_by_Y[current_floor] = []
 		for section in all_valid_sections:
+			
 			for loc in all_valid_sections[section]:
 				
-				array_of_locations.push_back(loc + Vector3i(0,current_floor,0) )
+				list_of_locations_by_Y[current_floor].push_back(loc + Vector3i(0,current_floor,0) )
 		current_floor += 1
-	for location in array_of_locations: # for each thing in array_of_locations create a Tile
-		var new_Tile = Tile.new()
+	for floor in list_of_locations_by_Y: # for each floor (which is an Array) in list_of_locations_by_Y
+		for location in floor: # make a tile for each
+			var new_Tile = Tile.new()
 		# populate the Tiles.neighbours array with the locations
+			
+			if floor != from:
+				# add neigbourhs above
+				pass
+			if floor != to:
+				# add neigbourhs below
+				pass
 		# remeber to add 2 excpetions - the first and last row, so Y = from and Y = to
 	
 
@@ -79,7 +158,7 @@ func collapse_tile_at_loc(location : Vector3i) -> void:
 	assert(!selected_tile.is_collpased)
 	selected_tile.state = selected_tile.valid_states[randi()%selected_tile.valid_states.size()]
 	selected_tile.is_collpased = true
-	
+
 	propagate_collapse(selected_tile.state,selected_tile.neighbours)
 	pass
 
