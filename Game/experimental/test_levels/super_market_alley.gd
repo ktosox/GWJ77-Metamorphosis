f3a0135d7ea65c4a56994fcb36764a63c1c79afc
Ktosox <ktoso3@gmail.com>
2025-02-22 19:47:09 +0100

    minor stuff

---
+++ Game/experimental/test_levels/super_market_alley.gd
@@ -0,0 +1,14 @@
+extends Node3D
+
+# starts with a door which the player crashes into
+# followed by a swapping loop of rows of A shleves and B shelves
+
+
+# Called when the node enters the scene tree for the first time.
+func _ready() -> void:
+	pass # Replace with function body.
+
+
+# Called every frame. 'delta' is the elapsed time since the previous frame.
+func _process(delta: float) -> void:
+	pass
